0326f302fcac56933b0caf51606466fe7acfd786
excellencetechnologies <excellencetechnologies08@gmail.com>
2017-07-24 17:41:28 +0530

    implement Go button functionality and include main page in navigation

--- src/Api/data.js
+++ src/Api/data.js
@@ -4,6 +4,7 @@ export const SUCCESS = {
     name: 'arun',
     email: 'arun@gmail.in',
     mobile_no: '0123456789',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSynJLjDmPA6KZ40sUVGRJSAc3LglJfQsh5DcoI8u41XDmnKY0Z',
     rounds: [
       {
         text: 'First Round',
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Image, View, AlertIOS, ActivityIndicator } from 'react-native';
+import { Image, View, AlertIOS, ActivityIndicator, Platform, ToastAndroid } from 'react-native';
 import { Container, Content, Text, Body, Button, Form, Item, Input, Label } from 'native-base';
 
+import * as data from '../../Api/data';
 import style from './styles';
 
 export default class MainPage extends Component {
@@ -11,6 +12,22 @@ export default class MainPage extends Component {
       email: '',
       isloading: false,
     };
+  }
+  handleSubmit() {
+    this.setState({ isloading: true });
+    if (this.state.email === 'arun@gmail.com') {
+      const success = data.SUCCESS;
+      this.setState({ isloading: false });
+      this.props.navigation.navigate('Drawer', { data: success });
+    } else {
+      this.setState({ isloading: false, email: '' });
+      const error = data.FAILED;
+      if (Platform.OS === 'android') {
+        ToastAndroid.showWithGravity(error.data.text, ToastAndroid.SHORT, ToastAndroid.BOTTOM);
+      } else if (Platform.OS === 'ios') {
+        AlertIOS.alert(error.data.text);
+      }
+    }
   }
   render() {
     return (
@@ -24,10 +41,10 @@ export default class MainPage extends Component {
           </View>
           <View style={style.content}>
             <Item floatingLabel>
-              <Label style={style.label}> Enter Your Email.......</Label>
-              <Input />
+              <Label style={{ marginLeft: 5, justifyContent: 'center' }}> Enter Your Email.......</Label>
+              <Input onChangeText={(text) => { this.setState({ email: text }); }} />
             </Item>
-            <Button rounded style={style.button}>
+            <Button rounded style={style.button} onPress={() => { this.handleSubmit(); }} >
               <Text style={{ alignSelf: 'center' }}>Go</Text>
             </Button>
           </View>
