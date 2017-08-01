bba29cd84abe04213065bf67a812009315e16a17
rohit <rohit.excel2011@gmail.com>
2017-08-01 11:16:42 +0530

    basic design completed for description of round with static data

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,7 +7,7 @@
 import React, { Component } from 'react';
 import CustomHeader from '../header/header';
 import { Container, Content, List, ListItem, Text, Body, Right, Icon } from 'native-base';
-import { View, AsyncStorage, FlatList, Platform } from 'react-native';
+import { View, AsyncStorage, FlatList, TouchableWithoutFeedback } from 'react-native';
 import style from './styles';
 import * as services from '../../api/services';
 
@@ -18,6 +18,7 @@ export default class HomePage extends Component {
       userinfo: '',
       username: '',
       refreshing: false,
+      isClicked: false,
     };
     this._drawerHandle = this._drawerHandle.bind(this);
     this._handleRefresh = this._handleRefresh.bind(this);
@@ -30,6 +31,14 @@ export default class HomePage extends Component {
       const rounds = JSON.parse(result);
       this.setState({ username: rounds, userinfo: rounds.rounds });
     });
+  }
+  onListItemPress(item) {
+    const roundMark = this.state.isClicked;
+    if (!roundMark && item.status == 1) {
+      this.setState({ isClicked: true });
+    } else if (roundMark && item.status == 1) {
+      this.setState({ isClicked: false });
+    }
   }
   _handleRefresh() {
     AsyncStorage.getItem('user', (err, result) => {
@@ -60,16 +69,48 @@ export default class HomePage extends Component {
             data={this.state.userinfo}
             refreshing={this.state.refreshing}
             onRefresh={() => { this._handleRefresh(); }}
-            renderItem={({ item, index }) => (<ListItem key={index} style={item.status == '1' ? style.selectedlistitem : style.listitem} >
-              <Body>
-                <Text>{item.text}</Text>
-                <Text note>{item.scheduled_date} {item.scheduled_date.length > 0 ? 'at' : null } {item.scheduled_time}</Text>
-              </Body>
-              {item.status == '1' ?
-                <Right style={style.listright}>
-                  <Icon name="star" active style={style.selected} />
-                </Right> : null}
-            </ListItem>)}
+            renderItem={({ item, index }) => (<View style={{ flex: 1 }}>
+              <View >
+                <ListItem key={index} onPress={() => { this.onListItemPress(item); }} style={item.status == '1' ? style.selectedlistitem : style.listitem} >
+                  <Body>
+                    <Text>{item.text}</Text>
+                    <Text note>{item.scheduled_date} {item.scheduled_date.length > 0 ? 'at' : null } {item.scheduled_time}</Text>
+                  </Body>
+                  {item.status == '1' ?
+                    <Right style={style.listright}>
+                      <Icon name="star" active style={style.selected} />
+                    </Right> : null}
+                </ListItem>
+              </View>
+              {this.state.isClicked == true && item.status == '1' ? <View style={style.itemDetails}>
+                <TouchableWithoutFeedback onPress={() => { this.onListItemPress(item); }}>
+                  <View style={style.viewMargin}>
+                    <Text style={style.jobtitle}>
+                      Job Description
+                    </Text>
+                    <Text style={style.viewMargin}>
+                      Here is job description for Designer Post
+                    </Text>
+                    <Text style={style.viewMargin}>
+                      Here is job description for Designer Post
+                      Exposure to develop next generation React Native apps with exciting possibilities on GeoLocation, Games, IoT integration etc.
+                      Opportunity to work with full life cycle of app development
+                      Work closely with creative and development team
+                      Opportunity to offer solution in app development problems
+                      You are passionate about programming, new technologies and solving problems. You are really serious about what and how you are learning
+                      A minimum of one year of professional React Native experience
+                      Have shipped few quality live Apps in App Store and Play Store.
+                      Solid understanding of Mobile application development life cycle.
+                      You are startup-minded, love open-source and working on complex products
+                      Excellent knowledge in working with web APIs and communicating with teams.
+                      Development experience with Native or Hybrid mobile apps especially capabilities, advantages and applicability of the Apps
+                      You are an independent, task oriented professional and can handle pressures of working in product teams with timeline pressures.
+                      Able to work both independently and collaboratively as needed.
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View> : null}
+            </View>)}
           />
         </View>
       </View>
