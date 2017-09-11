70247692e440dafc329d0a358e5b07e961589bb7
rohit <rohit.excel2011@gmail.com>
2017-09-11 14:07:33 +0530

    Added notification Icon && link Change && Configure firbase Performance

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://5.9.144.226:8090/';
+// axios.defaults.baseURL = 'http://5.9.144.226:8090/';
+// axios.defaults.baseURL = 'http://144.76.34.244:8090/';
+axios.defaults.baseURL = 'http://192.168.1.36:8091/';
 export const login = 'app_get_candidate';
 export const deviceData = 'app_save_candidate_device';
 export const logout = 'candidate/logout';
